30cb83b95c58e08ccacdab8cd9fa7976a8a68bcb
c2tao <b97901182@gmail.com>
2017-01-17 14:19:27 +0800

    normalized feature

--- timit_parse.py
+++ timit_parse.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import subprocess
+import cPickle
 root = '/home/c2tao/TIMIT/timit/'
 wav_root = '/home/c2tao/corpus/timit_sentence_wav/'
 word_root = '/home/c2tao/corpus/timit_word_wav/'
@@ -72,27 +73,6 @@ def timit_parse(name, dot):
     return int_list, tag_list 
 
 
-def timit_mlf(name, ostream, dot, dur):
-    ostream.write('"*/'+name+'.rec"\n')
-    int_list, tag_list = timit_parse(name, dot)
-    full = np.insert(int_list, 0, 0.0)
-    for i in range(len(tag_list)):
-        ostream.write('{} {} {} {}\n'.format(int(full[i]*dur)*100000, int(full[i+1]*dur)*100000, tag_list[i], 0.0))
-    ostream.write('.\n')    
-
-def timit_write(out_file, dot, opt):
-    timit_list = get_timit_list()
-    from zrst.util import MLF
-    mlf = MLF('timit_dummy_train.mlf')
-    mlf.merge(MLF('timit_dummy_test.mlf'))
-    print mlf.wav_list
-    dur_dict = dict(zip(mlf.wav_list, mlf.int_list))
-    with open(out_file, 'w') as f:
-        f.write('#!MLF!#\n')
-        for t in timit_list:
-            print t    
-            if opt in t:
-                timit_mlf(t, f, dot, dur_dict[t][-1])
 ##################################################################################
 # wav splice functions
 def timit_sox(name, folder, dot='wrd'):
@@ -101,15 +81,14 @@ def timit_sox(name, folder, dot='wrd'):
     int_list = np.insert(int_list, 0, 0)*total_sec
     print name, total_sec, int_list, tag_list
     #sox in.mp3 out.mp3 trim offset_in_secs duration_in_secs
-    for i, tag in enumerate(tag_list): 
+    for i, tag in enumerate(tag_list):
         out_wav = os.path.join(folder, '_'.join([name, tag])+'.wav')
         in_wav  = os.path.join(wav_root, name+'.wav')
         sts = subprocess.Popen("sox {} {} trim {} {}".format(in_wav, out_wav, int_list[i], int_list[i+1]-int_list[i]), shell=True).wait()
 
 def timit_splice_word(path):
     timit_list = get_timit_list()
-    for wav in timit_list:
-        timit_sox(wav, path) 
+    for wav in timit_list: timit_sox(wav, path) 
 
 ##################################################################################
 # query selection functions
@@ -133,7 +112,12 @@ def find_answ(query_list, document_list):
                 answ[i,j]=1.0
     return answ
 
-def query_filter_by_wordlen(query_list, n_min=4, n_max=1000):
+def query_filter_by_featlen(query_list, n_min=20, n_max=1000):
+    feat_len = map(lambda x: feat_extract(feat_name(x),1).shape[0], query_list)
+    selected = filter(lambda i:feat_len[i]>=n_min and feat_len[i]<= n_max , range(len(query_list)))
+    return map(lambda i: query_list[i], selected)
+
+def query_filter_by_wordlen(query_list, n_min=3, n_max=1000):
     word_list = map(last_word, query_list)
     selected = filter(lambda i:len(word_list[i])>=n_min and len(word_list[i])<=n_max, range(len(query_list)))
     return map(lambda i: query_list[i], selected)
@@ -143,32 +127,110 @@ def query_filter_by_occurence(query_list, document_list, n_min = 20, n_max =1000
     query_dist = np.sum(train_answ, axis=1)
     selected = filter(lambda i:query_dist[i]>=n_min and query_dist[i]<= n_max , range(len(query_list)))
     return map(lambda i: query_list[i], selected)
-
-def query_filter(query_list, document_list, min1, min2):
-    inx1 =query_filter_by_wordlen(query_list, min1)
-    inx2 =query_filter_by_occurence(query_list, document_list, min2)
-    selected = set(inx1).intersection(set(inx2))
-    print len(query_list), len(inx1), len(inx2), len(selected)
+     
+def query_filter(query_list, document_list, (min0,max0), (min1,max1), (min2,max2)):
+    inx0 =query_filter_by_featlen(query_list, min0, max0)
+    inx1 =query_filter_by_wordlen(query_list, min1, max1)
+    inx2 =query_filter_by_occurence(query_list, document_list, min2, max2)
+    #selected = sorted(set(inx1).intersection(set(inx2)))
+    selected = sorted(set(inx0).intersection(set(inx1)).intersection(set(inx2)))
+    print len(query_list),len(inx0), len(inx1), len(inx2), len(selected)
     return selected
 
-if __name__=='__main__':
-    timit_list = get_timit_list()
-    print timit_list[0]
-    print timit_parse(timit_list[0], 'phn')
-    print timit_dur(timit_list[0])/16000
+################################################################################################
+def feat_name(name):
+    if len(name.split('_'))==5:
+        return os.path.join(word_root, name)+'.wav'
+    elif len(name.split('_'))==4:
+        return os.path.join(wav_root, name)+'.wav'
     
-    #timit_splice_word('/home/c2tao/timit_word_corpus')
+def feat_extract(wav, nfilt=32):
+    from python_speech_features import mfcc, logfbank
+    from scipy.io import wavfile
+    #try:
+    if wav[-3:] !='wav': wav = feat_name(wav)
+    (rate,sig) = wavfile.read(wav)
+    fbank_feat = logfbank(sig,rate, nfilt=nfilt)
+    #except:
+    #    print wav
+    return fbank_feat
 
+def feat_view(feat):
+    import matplotlib.pyplot as plt
+    timit_path = '/home/c2tao/timit_mini_corpus/'
+    plt.matshow(feat)
+    plt.show()
+    
+def feat_mask(feat, nT = 256):
+    if nT:
+        diff = feat.shape[0]-nT
+        if diff>=0:
+            i = diff/2
+            return feat[i:i+nT,:], np.ones(nT)
+        else:
+            zero = np.zeros([nT, feat.shape[1]])
+            mask = np.zeros(nT)
+            i = -diff/2
+            zero[i:i+feat.shape[0],:] = feat
+            mask[i:i+feat.shape[0]] = 1.0
+            return zero, mask
+    else: 
+        return feat, np.ones(feat.shape[0])
+
+def feat_pack(wav_list, (u, v) = (0.0, 1.0), nT = None):
+    feats = []
+    masks = []
+    for wav in wav_list:
+        feat = feat_extract(feat_name(wav))
+        feat = (feat-u)/v
+        feat, mask= feat_mask(feat, nT)
+        feats.append(feat)
+        masks.append(mask)
+    feat_len = [f.shape[0] for f in feats]
+    print np.max(feat_len), np.mean(feat_len), np.std(feat_len)
+    return feats, masks
+
+def feat_norm(wav_list):
+    feats, ___ = feat_pack(wav_list, (0.0, 1.0), None)
+    concat_feat = np.concatenate(feats, axis=0)
+    u =  np.mean(concat_feat, axis = 0)
+    v =  np.std(concat_feat, axis = 0)
+    return u, v
+
+def feat_save(wav_list, pickle_file, (u, v)=(0.0, 1.0), nT = None):
+    with open(pickle_file,'wb') as f:
+        cPickle.dump(feat_pack(wav_list, (u, v), nT), f, protocol=-1)
+    
+def feat_load(pickle_file):
+    with open(pickle_file,'rb') as f:
+        return cPickle.load(f)
+
+ 
+if __name__=='__main__':
+    '''
+    timit_list = get_timit_list()
+    for wav in timit_list: 
+        timit_sox(wav, word_root) 
+    '''
     train_query = sorted(timit_filter(['dr8','train','_sx'], word_root))
     test_query = sorted(timit_filter(['dr8','test','_sx'], word_root))
     train_document = sorted(timit_filter(['train','_sx'], wav_root) - timit_filter(['train','_sx','dr8'], wav_root))
     test_document = sorted(timit_filter(['test','_sx'], wav_root) - timit_filter(['test','_sx','dr8'], wav_root))
      
-    train_query = query_filter(train_query, train_document, 3, 20)
-    test_query  = query_filter(test_query, test_document, 3, 20)
-    print train_query
-     
-    #timit_write('timit_train_wrd.mlf', 'wrd', 'train')
-    #timit_write('timit_test_wrd.mlf', 'wrd', 'test')
-    #timit_write('timit_train_phn.mlf', 'phn', 'train')
-    #timit_write('timit_test_phn.mlf', 'phn', 'test')
+    train_query = query_filter(train_query, train_document, (20,1000), (3,999), (20,999))
+    test_query  = query_filter(test_query, test_document, (20,1000), (3,999), (20,999))
+
+    u, v = feat_norm(train_query)
+    #feats, masks =  feat_pack(wav_list, (u,v), 30)
+    #feat_view(feats[0])
+    
+    #feat_save(train_query, 'feature/train_query.pkl', (u, v), None)
+    #feat_save(test_query, 'feature/test_query.pkl', (u, v), None) 
+    #feat_save(train_document, 'feature/train_document.pkl', (u, v), 512)
+    #feat_save(test_document, 'feature/test_document.pkl', (u, v), 512)
+    feat_save(train_document[:100], 'feature/mini_document.pkl', (u,v), 512)
+    
+    
+
+
+
---
+++ twist_timit.py
@@ -0,0 +1,196 @@
+from keras.layers import merge, Reshape, TimeDistributed, Lambda, Cropping1D, Input, Dense, Convolution2D, MaxPooling2D, UpSampling2D, Layer, BatchNormalization, Dropout, Flatten, Activation
+from keras.models import Model
+from keras.models import load_model
+from keras import backend as K
+from keras.regularizers import l1
+from keras_layers import Context, Twist
+import numpy as np
+# theano weight indexing: (n_input), n_row, n_col, n_output
+# theano tensor indexing: (n_batch), n_row, n_col, n_channel
+# my order n_batch, n_time, n_feat_sub, n_feat 
+n_post = 100
+n_embed = n_post
+n_context = 1
+n_batch = 5
+n_epoch = 100
+n_size = 5
+n_chan = 32
+ew_trans, ew_recon, ew_sharp = 0, 1, 0
+################################################################################
+'''
+from keras.datasets import mnist
+
+(x_train, _), (x_test, _) = mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+#x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))[:50,:,:,:]
+#x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+x_train = np.reshape(x_train, (len(x_train)/n_seq, n_seq*28, 28, 1))
+x_test = np.reshape(x_test, (len(x_test)/n_seq, n_seq*28, 28, 1))
+'''
+import timit_parse
+x_train_feat, x_train_mask = timit_parse.feat_load('feature/mini_document.pkl')
+x_train = np.array(x_train_feat)[:,:,:, np.newaxis] 
+ 
+################################################################################
+def dummy_objective(dummy_target, obj):
+    return obj
+
+def dummy_target(x):
+    return np.zeros(len(x))
+
+def dummy_shape(input_shapes):
+    assert len(input_shapes)==2
+    return input_shapes[0][0], 1
+################################################################################
+def obj_trans(inputs):
+    from keras.objectives import categorical_crossentropy, binary_crossentropy
+    y_answ, y_pred = inputs
+    return K.mean(categorical_crossentropy(y_answ, y_pred), axis=-1)*ew_trans
+
+def obj_sharp(inputs):
+    y_dist, _ = inputs
+    ep=1e-8
+    tot_prob = K.mean(y_dist, axis=-2)
+    tot_entro = K.mean(-tot_prob*K.log(tot_prob+ep), axis=-1)
+    ind_entro = K.mean(K.mean(-y_dist*K.log(y_dist+ep), axis=-1), axis=-1)
+    return (ind_entro-tot_entro)*ew_sharp
+
+def obj_recon(inputs):
+    y_answ, y_pred = inputs
+    return K.mean(K.mean(K.mean(K.square(y_answ - y_pred), axis=-1), axis=-1),axis=-1)*ew_recon
+
+def obj_final(inputs):
+    errs = K.stack(inputs[:-1])
+    weight = inputs[-1]
+    print K.flatten(K.dot(K.expand_dims(weight, 0),errs))
+    return K.flatten(K.dot(K.expand_dims(weight, 0),errs))
+################################################################################
+
+#def error_final(err_list, err_weights):
+def error_final(err_list):
+    #weights = K.stack(err_weights)
+    #weighted_error = map(lambda e,w:e*w, err_list, err_weights)
+    #err = merge(err_list+[weights], mode=obj_final, output_shape = dummy_shape, name = 'err_final')
+    err = merge(err_list, mode='sum', name = 'err_final')
+    return err
+
+def error_recon(input_feat, decoded):
+    err = merge([input_feat, decoded], mode=obj_recon, output_shape = dummy_shape, name = 'err_recon')
+    return err
+
+def error_sharp(p):
+    err = merge([p, p], mode=obj_trans, output_shape =  dummy_shape, name = 'err_sharp')
+    return err
+
+def error_trans(p, e):
+    e = Context(n_context)(p)
+    c = TimeDistributed(Dense(n_post, activation = 'softmax'), name = 'predicted')(e)
+    err = merge([p,c], mode=obj_trans, output_shape =  dummy_shape, name = 'err_trans')
+    #e = Context(n_context)(e)
+    #c = TimeDistributed(Dense(n_post, activation = 'softmax'), name = 'predicted')(e)
+    #err = merge([p,c], mode=obj_trans, output_shape =  dummy_shape, name = 'err_trans')
+    return err
+
+def layer_twist(x, mode='encoder', (cT, cF) = (2, 2), (nT, nF, nC)=(n_size,n_size,n_chan), w=None,name=None):
+    if not w: 
+        w = Convolution2D(nC, nT, nF, border_mode='same')
+    x = w(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    if mode=='encoder':
+        x = MaxPooling2D((cT, 1), border_mode='same')(x)
+        x = UpSampling2D((1, cF))(x)
+    elif mode=='decoder':
+        x = MaxPooling2D((1, cF), border_mode='same')(x)
+        x = UpSampling2D((cT, 1))(x)
+    x = Dropout(0.5, name = name)(x)
+    return x, w
+
+def layer_posterior(x, nP, nE):
+    nB, nT, nF, nC = K.int_shape(x)
+    x = Reshape((nT,nF*nC))(x)
+    p = TimeDistributed(Dense(nP, activation='softmax'), name = 'posterior')(x)   
+    e = TimeDistributed(Dense(nE, activation='tanh'), name = 'embedding')(p)   
+    return p, e
+
+def layer_reverse(e, x):
+    nB, nT, nF, nC = K.int_shape(x)
+    x = TimeDistributed(Dense(nF*nC, activation='tanh'))(e)
+    x = Reshape((nT, nF, nC))(x)
+    return x 
+
+def layer_rename(x, name = None):
+    nB, nT, nF, nC = K.int_shape(x)
+    x = Reshape((nT, nF, nC),name=name)(x)
+    return x
+################################################################################
+input_feat = Input(shape=(512, 32, 1), name = 'input')
+mask = Input(shape=(512, ), name = 'mask')
+x = input_feat
+
+x, _ = layer_twist(x, 'encoder', (4, 1))
+x, _ = layer_twist(x, 'encoder', (2, 2))
+x, _ = layer_twist(x, 'encoder', (2, 2))
+
+p, e = layer_posterior(x, n_post, n_embed)
+err_trans = error_trans(p, e)
+err_sharp = error_sharp(p)
+x = layer_reverse(e, x)
+
+x, _ = layer_twist(x, 'decoder', (2, 2))
+x, _ = layer_twist(x, 'decoder', (2, 2))
+x, _ = layer_twist(x, 'decoder', (4, 1))
+
+x = Convolution2D(1, 3, 3, activation='tanh', border_mode='same')(x)
+
+decoded = layer_rename(x, 'reconstructed')
+
+err_recon = error_recon(input_feat, decoded)
+err_final = error_final([err_trans, err_sharp, err_recon])
+
+tokenizer = Model(input_feat, err_final)
+tokenizer.compile(optimizer='adam', loss=dummy_objective)
+
+tokenizer.fit(x_train, dummy_target(x_train),
+                nb_epoch=n_epoch,
+                batch_size=n_batch,
+                shuffle=True,
+                validation_data=(x_train, dummy_target(x_train)),
+                callbacks=[])
+tokenizer.save('model/timit_mini.h5')
+#####################################################
+#tokenizer = load_model('model/timit_mini.h5', custom_objects = {'Context':Context,'dummy_objective':dummy_objective,'Twist':Twist})
+####################################################
+import matplotlib.pyplot as plt
+
+input_feat = tokenizer.get_layer("input").input
+autoencoder = Model(input_feat, tokenizer.get_layer("reconstructed").output)
+posterior = Model(input_feat, tokenizer.get_layer("posterior").output)
+recon_err = Model(input_feat, tokenizer.get_layer("err_recon").output)
+trans_err = Model(input_feat, tokenizer.get_layer("err_trans").output)
+decoded_imgs = autoencoder.predict(x_train)
+
+plt.figure(figsize=(20, 4))
+
+ax = plt.subplot(2, 1, 1)
+plt.imshow(x_train[0].reshape(512, 32).T)
+plt.gray()
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+
+ax = plt.subplot(2, 1, 2)
+plt.imshow(decoded_imgs[0].reshape(512, 32).T)
+plt.gray()
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+
+plt.show()
+#####################################################3
+plt.matshow(posterior.predict(x_train)[:1].reshape(-1,n_post).T)
+plt.show()
+
+rer = recon_err.predict(x_train)
+ter = trans_err.predict(x_train)
+print rer, ter
